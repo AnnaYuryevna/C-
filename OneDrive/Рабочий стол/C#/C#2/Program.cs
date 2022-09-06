06e08989c0d1faac3daae0e66277d0633fc30ec1
Anna <anechca-a@mail.ru>
2022-09-06 17:20:28 +0300

    Вычисление max из двух чисел

---
+++ OneDrive/Рабочий стол/C#/C#2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+int numberA=5;
+int numberB=7;
+int max = numberA;
+if (numberA>max) max = numberA;
+if (numberB>max) max = numberB;
+
+Console.Write("max =  ");
+Console.WriteLine(max);
