a2eab4b0c85b0c9cba5aa5425a4906fcd4e12f02
Anna <anechca-a@mail.ru>
2022-09-08 00:00:29 +0300

    Сохранила C#2.1

---
+++ OneDrive/Рабочий стол/C#/C#2.1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+int numberA=2;
+int numberB=10;
+int max = numberA;
+if (numberA>max) max = numberA;
+if (numberB>max) max = numberB;
+
+Console.Write("max =  ");
+Console.WriteLine(max);
