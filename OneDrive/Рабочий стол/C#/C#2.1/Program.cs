ea069ebb6973935454876c26bb1127fa01ddf014
Anna <anechca-a@mail.ru>
2022-09-08 16:02:23 +0300

    Нахождение max числа

--- OneDrive/Рабочий стол/C#/C#2.1/Program.cs
+++ OneDrive/Рабочий стол/C#/C#2.1/Program.cs
@@ -1,10 +1,12 @@
 ﻿//Напишите программу, которая на вход принимает два числа и выдаёт,
 // какое число большее, а какое меньшее.
-int numberA=2;
-int numberB=10;
-int max = numberA;
-if (numberA>max) max = numberA;
-if (numberB>max) max = numberB;
+Console.Write("Введите число: a= ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: b= ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max=a;
+if (b>max) max = b;
 
 Console.Write("max =  ");
 Console.WriteLine(max);
