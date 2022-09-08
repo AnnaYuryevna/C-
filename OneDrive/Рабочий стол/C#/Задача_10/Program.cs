adefaf3976f08e28ebaa7e70e1f9bb591c4fe236
Anna <anechca-a@mail.ru>
2022-09-08 17:58:00 +0300

    Задача 10 с исправлением

--- OneDrive/Рабочий стол/C#/Задача_10/Program.cs
+++ OneDrive/Рабочий стол/C#/Задача_10/Program.cs
@@ -4,5 +4,5 @@
 Console.Clear();
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-if (n>100 && n<1000);
+if (n>100 && n<1000)
 Console.Write(Convert.ToString(n)[1]);
---
+++ OneDrive/Рабочий стол/C#/Задача_13/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n<99)
+    Console.WriteLine("Третьей цыфры нет");
+else
+    Console.Write(Convert.ToString(n)[2]);
