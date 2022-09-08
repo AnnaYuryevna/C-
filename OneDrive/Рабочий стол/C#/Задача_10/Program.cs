f6aed1329f8e451cdc2ce1bc3f1fac9dbc0b87e8
Anna <anechca-a@mail.ru>
2022-09-08 16:45:02 +0300

    Задача_10/

---
+++ OneDrive/Рабочий стол/C#/Задача_10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программa, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n>100 && n<1000);
+Console.Write(Convert.ToString(n)[1]);
