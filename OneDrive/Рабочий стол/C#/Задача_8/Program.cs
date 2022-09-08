847b23253557d827e14975aa1b575e3ca1857aa1
Anna <anechca-a@mail.ru>
2022-09-08 20:33:03 +0300

    Задача_8

---
+++ OneDrive/Рабочий стол/C#/Задача_8/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int index = 2; index <= N; index += 2)
+    Console.Write(index + " ");
