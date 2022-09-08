c9bfae65b707979401b0be9c4d71d551524b5ee0
Anna <anechca-a@mail.ru>
2022-09-08 19:33:34 +0300

    Задача 6

---
+++ OneDrive/Рабочий стол/C#/Задача_6/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка)
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n%2 == 0)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
