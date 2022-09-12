9fc2a25d885882e759acd44a2c6d3c2f72dba07b
Anna <anechca-a@mail.ru>
2022-09-12 18:23:46 +0300

    Задача_19

---
+++ OneDrive/Рабочий стол/C#/Задача_19/Program.cs
@@ -0,0 +1,19 @@
+﻿//Программа, которая принимает на вход пятизначное число и проверяет,
+//является ли оно палиндромом
+
+Console.Clear();
+Console.WriteLine("Введите n1: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n2: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n3");
+int n3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n4: ");
+int n4 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n5: ");
+int n5 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 == n5 && n2 == n4)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
