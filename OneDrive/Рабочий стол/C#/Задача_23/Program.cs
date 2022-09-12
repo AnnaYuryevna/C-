349e4c747bcc564e58341963dd79cdd247c7a1b6
Anna <anechca-a@mail.ru>
2022-09-12 21:19:10 +0300

    Задача_23

---
+++ OneDrive/Рабочий стол/C#/Задача_23/Program.cs
@@ -0,0 +1,8 @@
+﻿//Программа, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N
+
+Console.Clear();
+Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for ( int index = 1; index <= n; index ++)
+Console.WriteLine (index + "^3 =" + index * index * index);
