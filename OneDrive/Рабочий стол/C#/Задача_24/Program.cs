79d6f7295c4aa6051e01c6ba9a948566e4e9c5f2
Anna <anechca-a@mail.ru>
2022-09-12 22:36:24 +0300

    Задача 24

---
+++ OneDrive/Рабочий стол/C#/Задача_24/Program.cs
@@ -0,0 +1,6 @@
+﻿//Программа, которая принимает на вход число (n) 
+//и выдает сумму чисел от 1 до n
+
+Console.WriteLine("Введите n: ");
+double n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(((n+1)/2)*n);
