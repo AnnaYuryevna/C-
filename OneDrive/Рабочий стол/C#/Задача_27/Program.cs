ef6040d4c0324e683d1d0da35783123ba895a32a
Anna <anechca-a@mail.ru>
2022-09-15 10:03:53 +0300

    Задача_27

---
+++ OneDrive/Рабочий стол/C#/Задача_27/Program.cs
@@ -0,0 +1,12 @@
+﻿//Программа, которая принимает на вход число 
+//и выдаёт сумму цифр в числе
+
+Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int summa = 0;
+while(n != 0)
+   {
+    summa += n % 10;
+    n /= 10;
+   }
+Console.WriteLine(summa);
