103626435176d9c0b0112332b4ff49537d20ab17
Anna <anechca-a@mail.ru>
2022-09-17 01:33:44 +0300

    Задача_29.1

---
+++ OneDrive/Рабочий стол/C#/Задача_29.1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Программа, которая задаёт массив из 8 элементов 
+//и выводит их на экран 
+
+Console.WriteLine("Введите массив, из 8 чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [8];
+for (int i =0; i < array.Length; i ++)
+array [i] = Convert.ToInt32(Console.ReadLine());
+Console.Write ("[",  string.Join(" , ", array), "]");
