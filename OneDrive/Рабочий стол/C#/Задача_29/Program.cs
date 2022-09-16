1a68274b51cba8443e50b9f2ff9bb97414ad3c39
Anna <anechca-a@mail.ru>
2022-09-16 06:48:14 +0300

    Задача 29

---
+++ OneDrive/Рабочий стол/C#/Задача_29/Program.cs
@@ -0,0 +1,11 @@
+﻿//Программа, которая задаёт массив
+// и выводит их на экран
+
+int n = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+int i = 1;
+while (i <= n)
+{
+    res = res * i; i++;
+}
+Console.WriteLine(res);
